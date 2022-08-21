1e68db7083c787043a6a11e2eab4e9abb9407bec
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-08-21 17:54:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array,value){
+  return(array.filter(function(arr){
+    return (arr.toUpperCase()===value.toUpperCase());
+  }))
+}
+
+function fuzzyMatch(array,value){
+  return (array.filter(function(arr){
+    return(arr.slice(0,2)===value)
+  }))
+}
+function matchName(array,value){
+  return (array.filter(function(arr){
+    return (arr.name===value)
+  }))
+}
